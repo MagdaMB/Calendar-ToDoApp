2f66ae82ba291a225abdab54116e31e0e2b3ce54
MagdaMB <magda.m.badura@gmail.com>
2019-03-01 21:18:09 +0100

    second version

--- src/App.js
+++ src/App.js
@@ -7,42 +7,53 @@ class Calendar extends Component {
     date: ""
   };
 
-  handle = () => {
-    console.log(this);
+  handleClick = () => {
     const text = document.querySelector(".topic").value;
-    console.log(text)
-
     const date = document.querySelector(".date").value;
-    console.log(date)
 
     this.setState({
       topic: this.state.topic + text,
       date: this.state.date + date
-    })
+    });
   };
 
   render() {
     return (
       <div className="app">
         <h1>Wprowadź zadania do wykonania:</h1>
-        <h3>Wprowadź krótki opis zadania </h3>
-        <input
-          type="text"
-          placeholder="Podaj krótki opis zadania"
-          className="topic"
-        />
-
-        <h3>Podaj datę</h3>
-        <input type="date" className="date" />
-        <button type="button" onClick={this.handle}>
+        <Description />
+        <Date />
+        <button type="button" onClick={this.handleClick}>
           Dodaj nowe zadanie
         </button>
 
-        <p className="result">{this.state.topic}-{this.state.date}</p>
+        <p className="resut">
+          {this.state.topic} {this.state.date}
+        </p>
       </div>
     );
   }
 }
 
+const Description = () => {
+  return (
+    <>
+      <h3>Podaj krótki opis </h3>
+      <input
+        type="text"
+        placeholder="Podaj krótki opis zadania"
+        className="topic"
+      />
+    </>
+  );
+};
 
+const Date = () => {
+  return (
+    <>
+      <h3>Podaj datę</h3>
+      <input type="date" className="date" />
+    </>
+  );
+};
 export default Calendar;
